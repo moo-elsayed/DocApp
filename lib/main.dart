64feb8ca125145c9af9227dc2477082fe8e19465
Mohamed Elsayed <elmoosayed@gmail.com>
2025-07-26 08:39:24 +0300

    - change app icon - add splash screen - change app name - fix text being hidden bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/core/routing/app_router.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'doc_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(appRouter: AppRouter()));
 }
